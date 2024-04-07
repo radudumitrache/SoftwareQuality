f6e1d552ced6aa3060a432495ef6c96051c2d4e3
Stefan Ionut Tasca <stefan.tasca@student.nhlstenden.com>
2024-04-07 18:44:28 +0200

    Refactor SlideItem interface in SlideItem.java

--- src/TextItem.java
+++ src/TextItem.java
@@ -14,23 +14,16 @@ import java.util.List;
 import java.util.Iterator;
 import java.util.ArrayList;
 
-/** <p>A tekst item.</p>
- * <p>A TextItem has drawingfunctionality.</p>
- * @author Ian F. Darwin, ian@darwinsys.com, Gert Florijn, Sylvia Stuurman
- * @version 1.1 2002/12/17 Gert Florijn
- * @version 1.2 2003/11/19 Sylvia Stuurman
- * @version 1.3 2004/08/17 Sylvia Stuurman
- * @version 1.4 2007/07/16 Sylvia Stuurman
- * @version 1.5 2010/03/03 Sylvia Stuurman
- * @version 1.6 2014/05/16 Sylvia Stuurman
- */
-
 public class TextItem implements SlideItem {
 	private String text;
 
 	StyleType styleType ;
 	private static final String EMPTYTEXT = "No Text Given";
 
+	public TextItem(String text) {
+        this.text = text;
+    }
+	
 	public StyleType getStyleType()
 	{
 		return styleType;
---
+++ src/TextItemCreator.java
@@ -0,0 +1,6 @@
+public class TextItemCreator implements SlideItemCreator {
+    @Override
+    public SlideItem createItemSlide() {
+        return new TextItem("Some default text");
+    }
+}
