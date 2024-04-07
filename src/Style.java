1a2d0b23297a0565378e980ee9780214d611ed87
radudumitrache <radu.dumitrache@student.nhlstenden.com>
2024-04-07 13:53:10 +0200

    added the builder classes

--- src/Style.java
+++ src/Style.java
@@ -14,45 +14,86 @@ import java.awt.Font;
  * @version 1.6 2014/05/16 Sylvia Stuurman
  */
 
-public class Style {
+public abstract class Style {
 	private static Style[] styles; // de styles
-	
 	private static final String FONTNAME = "Helvetica";
-	int indent;
-	Color color;
-	Font font;
-	int fontSize;
-	int leading;
+	private  int  indent;
+	private Color color;
+	private int fontSize;
+	private int leading;
+	private Font font;
 
-	public static void createStyles() {
-		styles = new Style[5];    
-		// The styles are fixed.
-		styles[0] = new Style(0, Color.red,   48, 20);	// style for item-level 0
-		styles[1] = new Style(20, Color.blue,  40, 10);	// style for item-level 1
-		styles[2] = new Style(50, Color.black, 36, 10);	// style for item-level 2
-		styles[3] = new Style(70, Color.black, 30, 10);	// style for item-level 3
-		styles[4] = new Style(90, Color.black, 24, 10);	// style for item-level 4
-	}
 
-	public static Style getStyle(int level) {
-		if (level >= styles.length) {
-			level = styles.length - 1;
-		}
-		return styles[level];
-	}
-
-	public Style(int indent, Color color, int points, int leading) {
+//	public static void createStyles() {
+//		styles = new Style[5];
+//		// The styles are fixed.
+//		styles[0] = new Style(0, Color.red,   48, 20);	// style for item-level 0
+//		styles[1] = new Style(20, Color.blue,  40, 10);	// style for item-level 1
+//		styles[2] = new Style(50, Color.black, 36, 10);	// style for item-level 2
+//		styles[3] = new Style(70, Color.black, 30, 10);	// style for item-level 3
+//		styles[4] = new Style(90, Color.black, 24, 10);	// style for item-level 4
+//	}
+	public Style(int indent, Color color, int fontSize, int leading) {
 		this.indent = indent;
 		this.color = color;
-		font = new Font(FONTNAME, Font.BOLD, fontSize=points);
+		font = new Font(FONTNAME, Font.BOLD, fontSize=fontSize);
 		this.leading = leading;
 	}
 
 	public String toString() {
 		return "["+ indent + "," + color + "; " + fontSize + " on " + leading +"]";
 	}
-
 	public Font getFont(float scale) {
 		return font.deriveFont(fontSize * scale);
+	}
+
+	public int getIndent()
+	{
+		return indent;
+	}
+
+	public void setIndent(int indent)
+	{
+		this.indent = indent;
+	}
+
+	public Color getColor()
+	{
+		return color;
+	}
+
+	public void setColor(Color color)
+	{
+		this.color = color;
+	}
+
+	public int getLeading()
+	{
+		return leading;
+	}
+
+	public void setLeading(int leading)
+	{
+		this.leading = leading;
+	}
+
+	public int getFontSize()
+	{
+		return fontSize;
+	}
+
+	public void setFontSize(int fontSize)
+	{
+		this.fontSize = fontSize;
+	}
+
+	public Font getFont()
+	{
+		return font;
+	}
+
+	public void setFont(Font font)
+	{
+		this.font = font;
 	}
 }
