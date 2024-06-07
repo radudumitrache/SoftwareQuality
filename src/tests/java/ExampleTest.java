3de2408a100905e40edba8a20b3b787b9f83950c
Stefan Ionut Tasca <stefan.tasca@student.nhlstenden.com>
2024-06-07 16:57:13 +0200

    adding an easy test

---
+++ src/tests/java/ExampleTest.java
@@ -0,0 +1,12 @@
+package com.softwarequality;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ExampleTest {
+
+    @Test
+    public void testExample() {
+        assertTrue(true);
+    }
+}
